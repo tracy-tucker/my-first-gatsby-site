9447d79df594274e2f42502a3bfbbd4e40a63b14
ttucker <tracytuckeremail@gmail.com>
2021-11-26 08:53:14 -0600

    Creating 3 empty blogs as a start

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -29,6 +29,7 @@ const Layout = ({pageTitle, children}) => {
                 <ul className={navLinks}>
                     <li className={navLinkItem}><Link className={navLinkText} to="/">Home</Link></li>
                     <li className={navLinkItem}><Link className={navLinkText} to="/about">About</Link></li>
+                    <li className={navLinkItem}><Link className={navLinkText} to="/blog">Blog</Link></li>
                 </ul>
             </nav>
             <main>
---
+++ src/pages/blog.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const BlogPage = () => {
+    return (
+        <div>
+            <Layout pageTitle="My Blog Posts">
+                <p>My cool posts will go in here</p>
+            </Layout>
+        </div>
+    )
+}
+
+export default BlogPage
