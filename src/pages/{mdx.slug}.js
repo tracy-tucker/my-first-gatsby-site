1381b1897d45afac60b6ee462686b384e06a3aca
ttucker <tracytuckeremail@gmail.com>
2021-11-27 07:43:10 -0600

    Adding the beginnings of the blog post template

---
+++ src/pages/{mdx.slug}.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const BlogPost = () => {
+    return (
+        <Layout pageTitle="Super Cool Blog Posts">
+            <p>My blog contents will go here (eventually).</p>
+        </Layout>
+    )
+}
+
+export default BlogPost
